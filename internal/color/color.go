51ec72fe678101496757cad1fe24062f6c7d383c
Kyosuke Fujimoto <kyoro.f@gmail.com>
2022-04-10 13:13:29 +0900

    Implement color page

--- internal/color/color.go
+++ internal/color/color.go
@@ -1,6 +1,63 @@
 package color
 
+import (
+	"sort"
+
+	"github.com/lucasb-eyer/go-colorful"
+)
+
 type Color struct {
 	ID  int
 	Hex string
+}
+
+type Distance int
+
+const (
+	RGB Distance = iota
+	CIE76
+	CIE94
+	CIEDE2000
+)
+
+type cd struct {
+	c Color
+	d float64
+}
+
+func FilterColor(hex string, dist Distance, n int) []Color {
+	target, err := colorful.Hex("#" + hex)
+	if err != nil {
+		return nil
+	}
+	f := distanceFunc(target, dist)
+	if f == nil {
+		return nil
+	}
+	cds := make([]cd, len(Cols))
+	for i, col := range Cols {
+		c, _ := colorful.Hex(col.Hex)
+		d := f(c)
+		cds[i] = cd{col, d}
+	}
+	sort.Slice(cds, func(i, j int) bool { return cds[i].d < cds[j].d })
+	ret := make([]Color, n)
+	for i := 0; i < n; i++ {
+		ret[i] = cds[i].c
+	}
+	return ret
+}
+
+func distanceFunc(c colorful.Color, dist Distance) func(colorful.Color) float64 {
+	switch dist {
+	case RGB:
+		return c.DistanceRgb
+	case CIE76:
+		return c.DistanceCIE76
+	case CIE94:
+		return c.DistanceCIE94
+	case CIEDE2000:
+		return c.DistanceCIEDE2000
+	}
+	return nil
 }
--- internal/ui/page_color.go
+++ internal/ui/page_color.go
@@ -4,25 +4,69 @@ import (
 	"bytes"
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/lusingander/btox/internal/color"
 )
 
+var (
+	colorPageItemStyle = lipgloss.NewStyle().
+				Padding(1, 2)
+
+	colorPageSelectedItemColorStyle = lipgloss.NewStyle().
+					Foreground(selectedColor).
+					Bold(true)
+
+	colorPageDisabledItemColorStyle = lipgloss.NewStyle().
+					Foreground(disabledColor)
+)
+
+const (
+	colorPageCountMin = 1
+	colorPageCountMax = 10
+)
+
 type colorPageModel struct {
-	listView viewport.Model
+	listView   viewport.Model
+	colorInput textinput.Model
 
 	delegateKeys colorPageDelegateKeyMap
+
+	selected colorPageSelectableItems
+	dist     colorDistance
+	count    int
 
 	width, height int
 }
 
-func colorPageColorListContent() string {
+type colorDistance int
+
+const (
+	colorDistanceRGB colorDistance = iota
+	colorDistanceCIE76
+	colorDistanceCIE94
+	colorDistanceCIEDE2000
+	colorDistanceNumberOfItems // not item
+)
+
+type colorPageSelectableItems int
+
+const (
+	colorPageSelectableInput colorPageSelectableItems = iota
+	colorPageSelectableDistance
+	colorPageSelectableCount
+	colorPageSelectableFilter
+	colorPageSelectableNumberOfItems // not item
+)
+
+func colorPageColorListContent(cols []color.Color) string {
 	var buf bytes.Buffer
-	for _, col := range color.Cols {
+	for _, col := range cols {
 		rect := lipgloss.NewStyle().
 			Width(6).
 			Background(lipgloss.Color(strconv.Itoa(col.ID)))
@@ -34,21 +78,53 @@ func colorPageColorListContent() string {
 func newColorPageModel() colorPageModel {
 	m := colorPageModel{}
 	m.listView = viewport.New(0, 0)
-	m.listView.SetContent(colorPageColorListContent())
+	m.colorInput = textinput.New()
+	m.colorInput.CharLimit = 6
+	m.colorInput.Placeholder = "000000"
+	m.colorInput.Prompt = ">  "
+	m.colorInput.PromptStyle = colorPageSelectedItemColorStyle
 	m.delegateKeys = newColorPageDelegateKeyMap()
 	m.reset()
 	return m
 }
 
 type colorPageDelegateKeyMap struct {
-	back key.Binding
+	enter, back, tab, shiftTab, h, l, j, k key.Binding
 }
 
 func newColorPageDelegateKeyMap() colorPageDelegateKeyMap {
 	return colorPageDelegateKeyMap{
+		enter: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", ""),
+		),
 		back: key.NewBinding(
 			key.WithKeys("backspace", "ctrl+h"),
 			key.WithHelp("backspace", ""),
+		),
+		tab: key.NewBinding(
+			key.WithKeys("tab"),
+			key.WithHelp("tab", ""),
+		),
+		shiftTab: key.NewBinding(
+			key.WithKeys("shift+tab"),
+			key.WithHelp("shift+tab", ""),
+		),
+		h: key.NewBinding(
+			key.WithKeys("h", "left"),
+			key.WithHelp("h", ""),
+		),
+		l: key.NewBinding(
+			key.WithKeys("l", "right"),
+			key.WithHelp("l", ""),
+		),
+		j: key.NewBinding(
+			key.WithKeys("j", "down"),
+			key.WithHelp("j", ""),
+		),
+		k: key.NewBinding(
+			key.WithKeys("k", "up"),
+			key.WithHelp("k", ""),
 		),
 	}
 }
@@ -57,33 +133,192 @@ func (m *colorPageModel) setSize(w, h int) {
 	m.width, m.height = w, h
 
 	m.listView.Width = w
-	m.listView.Height = h
+	m.listView.Height = h - lipgloss.Height(m.menuView()) - lipgloss.Height(m.separetorView()) - 1
 }
 
 func (m *colorPageModel) reset() {
+	m.selected = colorPageSelectableInput
+	m.dist = colorDistanceCIEDE2000
+	m.count = 5
+	m.colorInput.Reset()
+	m.colorInput.Focus()
+	m.listView.SetContent(colorPageColorListContent(color.Cols))
 	m.listView.GotoTop()
+}
+
+func (m *colorPageModel) selectItem(reverse bool) {
+	n := colorPageSelectableNumberOfItems
+	if reverse {
+		m.selected = ((m.selected-1)%n + n) % n
+	} else {
+		m.selected = (m.selected + 1) % n
+	}
+	if m.selected == colorPageSelectableInput {
+		m.colorInput.Focus()
+		m.colorInput.PromptStyle = colorPageSelectedItemColorStyle
+	} else {
+		m.colorInput.Blur()
+		m.colorInput.PromptStyle = lipgloss.Style{}
+	}
+}
+
+func (m *colorPageModel) switchSelectedItem(left bool) {
+	switch m.selected {
+	case colorPageSelectableDistance:
+		n := colorDistanceNumberOfItems
+		if left {
+			m.dist = ((m.dist-1)%n + n) % n
+		} else {
+			m.dist = (m.dist + 1) % n
+		}
+	case colorPageSelectableCount:
+		if left && m.count > colorPageCountMin {
+			m.count--
+		} else if !left && m.count < colorPageCountMax {
+			m.count++
+		}
+	default:
+		// do nothing
+	}
+}
+
+func (m *colorPageModel) filter() {
+	var d color.Distance
+	switch m.dist {
+	case colorDistanceRGB:
+		d = color.RGB
+	case colorDistanceCIE76:
+		d = color.CIE76
+	case colorDistanceCIE94:
+		d = color.CIE94
+	case colorDistanceCIEDE2000:
+		d = color.CIEDE2000
+	}
+	filtered := color.FilterColor(m.colorInput.Value(), d, m.count)
+	if filtered == nil {
+		m.listView.SetContent(colorPageColorListContent(color.Cols))
+		return
+	}
+	m.listView.SetContent(colorPageColorListContent(filtered))
 }
 
 func (m colorPageModel) Init() tea.Cmd {
 	return nil
 }
 
+type colorPageFilterMsg struct{}
+
+func colorPageFilter() tea.Msg {
+	return colorPageFilterMsg{}
+}
+
 func (m colorPageModel) Update(msg tea.Msg) (colorPageModel, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
+		case key.Matches(msg, m.delegateKeys.enter):
+			if m.selected == colorPageSelectableFilter {
+				return m, colorPageFilter
+			}
+			return m, nil
 		case key.Matches(msg, m.delegateKeys.back):
-			return m, goBack
+			if m.selected != colorPageSelectableInput {
+				return m, goBack
+			}
+		case key.Matches(msg, m.delegateKeys.tab):
+			m.selectItem(false)
+			return m, nil
+		case key.Matches(msg, m.delegateKeys.shiftTab):
+			m.selectItem(true)
+			return m, nil
+		case key.Matches(msg, m.delegateKeys.h):
+			m.switchSelectedItem(true)
+			return m, nil
+		case key.Matches(msg, m.delegateKeys.l):
+			m.switchSelectedItem(false)
+			return m, nil
+		case key.Matches(msg, m.delegateKeys.j):
+			m.selectItem(false)
+			return m, nil
+		case key.Matches(msg, m.delegateKeys.k):
+			m.selectItem(true)
+			return m, nil
 		}
 	case selectColorMenuMsg:
 		m.reset()
 		return m, nil
+	case colorPageFilterMsg:
+		m.filter()
+		return m, nil
 	}
 	var cmd tea.Cmd
-	m.listView, cmd = m.listView.Update(msg)
-	return m, cmd
+	if m.selected == colorPageSelectableInput {
+		m.colorInput, cmd = m.colorInput.Update(msg)
+		return m, cmd
+	} else {
+		m.listView, cmd = m.listView.Update(msg)
+		return m, cmd
+	}
 }
 
 func (m colorPageModel) View() string {
-	return m.listView.View()
+	menu := m.menuView()
+	sep := m.separetorView()
+	return lipgloss.JoinVertical(0, menu, sep, m.listView.View())
+}
+
+func (m colorPageModel) menuView() string {
+	s := ""
+
+	input := ""
+	input += m.colorInput.View()
+	s += colorPageItemStyle.Render(input)
+
+	var dist string
+	switch m.dist {
+	case colorDistanceRGB:
+		dist = "    RGB    "
+	case colorDistanceCIE76:
+		dist = "   CIE76   "
+	case colorDistanceCIE94:
+		dist = "   CIE94   "
+	case colorDistanceCIEDE2000:
+		dist = " CIEDE2000 "
+	}
+	s += colorPageItemStyle.Render(m.withStyle(dist, m.selected == colorPageSelectableDistance, false, false))
+
+	count := fmt.Sprintf("     %2d    ", m.count)
+	s += colorPageItemStyle.Render(m.withStyle(count, m.selected == colorPageSelectableCount, m.count <= colorPageCountMin, m.count >= colorPageCountMax))
+
+	filter := "     Filter    "
+	if m.selected == colorPageSelectableFilter {
+		filter = colorPageSelectedItemColorStyle.Render(filter)
+	}
+	s += colorPageItemStyle.Render(filter)
+
+	return s
+}
+
+func (m colorPageModel) separetorView() string {
+	sep := strings.Repeat("-", m.width)
+	return colorPageDisabledItemColorStyle.Render(sep)
+}
+
+func (colorPageModel) withStyle(s string, selected, first, last bool) string {
+	l := "<"
+	r := ">"
+	if first {
+		l = colorPageDisabledItemColorStyle.Render(l)
+	} else if selected {
+		l = colorPageSelectedItemColorStyle.Render(l)
+	}
+	if last {
+		r = colorPageDisabledItemColorStyle.Render(r)
+	} else if selected {
+		r = colorPageSelectedItemColorStyle.Render(r)
+	}
+	if selected {
+		s = colorPageSelectedItemColorStyle.Render(s)
+	}
+	return fmt.Sprintf("%s %s %s", l, s, r)
 }
